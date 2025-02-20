e5a1a5a5a7081ffc0bcd8f55026f9a5305459cdf
Martin Zane <martin@zaneweb.com>
2025-02-20 10:17:08 +0100

    Send different errors based on the environment

--- apps/api/src/middleware/error.ts
+++ apps/api/src/middleware/error.ts
@@ -1,20 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
 import AppError from '../utils/app-error';
 
+const sendErrorDev = (err: AppError, res: Response) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err: AppError, res: Response) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR: ', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 const errorHandler = (
   err: AppError,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const statusCode = err.statusCode || 500;
-  const status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-  res.status(statusCode).json({
-    status,
-    data: null,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    sendErrorProd(err, res);
+  }
 };
 
 export default errorHandler;
