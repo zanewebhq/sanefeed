a8b3f806fca5261eec27825ca0d763a1e7376292
Martin Zane <martin@zaneweb.com>
2025-03-05 11:51:15 +0100

    Generate verification code & expiration on signup

--- apps/api/src/controllers/auth.ts
+++ apps/api/src/controllers/auth.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 import { pool } from '../database';
 import catchAsync from '../utils/catch-async';
+import dayjs from 'dayjs';
 
 const jwtOptions = {
   secretOrKey: process.env.JWT_SECRET,
@@ -11,10 +13,12 @@ const jwtOptions = {
 export const signup = catchAsync(async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
+  const verificationCode = crypto.randomBytes(3).toString('hex').toUpperCase();
+  const verificationCodeExpiresAt = dayjs().add(1, 'hour').toISOString();
 
   const result = await pool.query(
-    'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *',
-    [email, hashedPassword]
+    'INSERT INTO users (email, password, verification_code, verification_code_expires_at) VALUES ($1, $2, $3, $4) RETURNING *',
+    [email, hashedPassword, verificationCode, verificationCodeExpiresAt]
   );
 
   const payload = { id: result.rows[0].id };
--- apps/api/src/routes/auth.ts
+++ apps/api/src/routes/auth.ts
@@ -3,8 +3,8 @@ import controllers from '../controllers';
 
 const router = Router();
 
-router.post('/signup', controllers.auth.signup);
-router.post('/login', controllers.auth.login);
-router.get('/logout', controllers.auth.logout);
+router.post('/auth/signup', controllers.auth.signup);
+router.post('/auth/login', controllers.auth.login);
+router.get('/auth/logout', controllers.auth.logout);
 
 export default router;
