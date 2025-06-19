514ea75e23381a29ad2c9a106df245b5c1d0d5b9
Martin Zane <martin@zaneweb.com>
2025-06-19 19:54:24 +0200

    Update recover method to update password

--- apps/api/src/controllers/auth/recover/index.ts
+++ apps/api/src/controllers/auth/recover/index.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
 import catchAsync from '../../../utils/catch-async';
 import { pool } from '../../../database';
 
 import { StatusCodes } from 'http-status-codes';
+import { User } from '../../../types';
+import dayjs from 'dayjs';
 
 export const recover = catchAsync(async (req: Request, res: Response) => {
   const { email, code, password } = req.body;
@@ -11,16 +14,33 @@ export const recover = catchAsync(async (req: Request, res: Response) => {
     email,
   ]);
 
-  const user = users.rows.at(0);
-
-  console.log(user);
+  const user = users.rows.at(0) as User;
 
   if (!user) {
     return res.status(StatusCodes.BAD_REQUEST).json({
       status: 'error',
-      message: 'User is already verified.',
+      message: 'Unable to recover password.',
     });
   }
+
+  const currentTime = dayjs();
+  const recoveryCodeExpiresAt = dayjs(user.recovery_code_expires_at);
+  const hasExpired = currentTime.isAfter(recoveryCodeExpiresAt);
+  const isMatching = code === user.recovery_code;
+
+  if (hasExpired || !isMatching) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      status: 'error',
+      message: 'Unable to recover password.',
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await pool.query(
+    'UPDATE users SET password = $1, recovery_code = NULL, recovery_code_expires_at = NULL WHERE email = $2 RETURNING *',
+    [hashedPassword, user.email]
+  );
 
   res.status(StatusCodes.OK).json({
     status: 'success',
